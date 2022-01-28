59a3dd639635256258fe0f08d04581c398227721
Michael-F-Bryan <michael@hotg.ai>
2022-01-29 02:42:33 +0800

    Switching mobilenet over from the enterprise proxy to the public forge backend

--- react-tfjs-mobilenet/index.tsx
+++ react-tfjs-mobilenet/index.tsx
@@ -1,44 +1,24 @@
-import "core-js"; // we need to polyfill things like Array.at()
+import "@tensorflow/tfjs"; // Register tfjs kernels
 
 import { useState, useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 import Webcam from "react-webcam";
-import  "@tensorflow/tfjs"
-import backend from "@hotg-ai/rune-tfjs-v2";
-import { Parameters, ForgeHook, useForge, registerBackend, OutputValue } from "@hotg-ai/forge";
+import backend from "@hotg-ai/rune-tfjs-v3";
+import { Parameters, useForge, registerBackend, OutputValue } from "@hotg-ai/forge";
 
-
-
-registerBackend(backend)
-
-
-
-const apiKey = process.env.REACT_APP_API_KEY;
-if (!apiKey) {
-    throw new Error(`The "REACT_APP_API_KEY" environment variable wasn't set`);
-}
-const deploymentId = process.env.REACT_APP_DEPLOYMENT_ID;
-if (!deploymentId) {
-    throw new Error(
-        `The "REACT_APP_DEPLOYMENT_ID" environment variable wasn't set`,
-    );
-}
-
+registerBackend(backend());
 
 const forgeConfig: Parameters = {
-    apiKey,
-    deploymentId: parseInt(deploymentId), // <--- Rune holds the model
-    // onLoadCallback: () => {}  When there is update
-    baseURL: "https://dev-enterprise-proxy.hotg.ai",
+    apiKey: "de71df64296fd8e39a81e7d24d1870b861442a31",
+    deploymentId: 17,
+    baseURL: "https://dev-forge.hotg.ai",
     telemetry: {
-        baseURL: "https://dev-enterprise-proxy.hotg.ai",
+        baseURL: "https://dev-telemetry.hotg.ai",
     },
-    backend: "enterprise"
 };
 
-
 export default function App() {
-    const [result, setResult] = useState<OutputValue[] | undefined>();
+    const [result, setResult] = useState<OutputValue[]>([]);
     const [image, setImage] = useState<string | undefined>();
     const forge = useForge(forgeConfig);
     const webcamRef = useRef<Webcam | null>(null);
@@ -52,27 +32,20 @@ export default function App() {
 
             img.onload = () => {
                 if (img.src) {
-
-                    // Forge should also provide the model
                     try {
-                        // debugger;
-                        const result = forge.predict({ floatImage: [img] });
-
-                        setResult(result);
+                        setResult(forge.predict({ floatImage: [img] }));
                     } catch (error) {
                         console.log(error)
                     }
                 }
-
             }
-            img.src = image
-            // 
+            img.src = image;
         }
     }, [image, forge]);
 
     const videoConstraints = {
-        width: 640,
-        height: 480,
+        height: 300,
+        width: 400,
         facingMode: "user",
     };
 
@@ -88,75 +61,25 @@ export default function App() {
         }
     };
 
+    const predictions = result.flatMap(v => [...v.elements])
+        .map((p, i) => (<li key={i}>{p}</li>));
+
     return (
         <div className="App">
-            <header className="App-header">
-                <Webcam
-                    audio={false}
-                    height={640}
-                    width={800}
-                    ref={webcamRef}
-                    screenshotFormat="image/jpeg"
-                    videoConstraints={videoConstraints}
-                    onTimeUpdate={onTimeUpdated}
-                />
+            <h1>Mobilenet ({forge.state})</h1>
+            <Webcam
+                audio={false}
+                height={videoConstraints.height}
+                width={videoConstraints.width}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                videoConstraints={videoConstraints}
+                onTimeUpdate={onTimeUpdated}
+            />
 
-                <Body forge={forge} result={result} />
-            </header>
-        </div>
+            <ul>{predictions}</ul>
+        </div >
     );
-}
-
-type BodyProps = {
-    forge: ForgeHook;
-    result: OutputValue[] | undefined;
-};
-
-/**
- * Render a different body depending on whether Forge is loaded or not.
- */
-function Body({ forge, result }: BodyProps) {
-    switch (forge.state) {
-        case "not-loaded":
-        case "loading":
-            return (
-                <div>
-                    <p>Loading...</p>
-                </div>
-            );
-
-        case "loaded":
-            const results = result?.map(r =>
-                JSON.stringify(r.elements, null, 2),
-            );
-            return (
-                <div>
-                    <h3>Result</h3>
-                    <pre style={{ textAlign: "left", width: "50vw" }}>
-                        {results}
-                    </pre>
-
-                </div>
-            );
-
-        case "error":
-            const { error } = forge;
-            const msg = error
-                ? `${error.name}: ${error.message}`
-                : "An error occurred...";
-            const stack = error
-                ? `${error.stack}`
-                : "Error stack unavailable";
-
-            return (
-                <div>
-                    <p>{msg}</p>
-                    <pre style={{ textAlign: "left", width: "50vw" }}>
-                        {stack}
-                    </pre>
-                </div>
-            );
-    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
